4a14c1049fde88998824124d4aa82bc273ee0b65
etowageed <etowagideon@gmail.com>
2023-03-22 00:07:51 +0100

    fixed button position issues

--- src/components/AdviceSlip.vue
+++ src/components/AdviceSlip.vue
@@ -3,7 +3,7 @@
     class="w-11/12 h-2/4 md:w-2/6 bg-DarkGrayishBlue shadow-2xl mx-auto rounded-lg p-5 mt-24 md:mt-0 flex flex-col justify-between">
 
     <div class=" text-center">
-      <span class="text-xs text-NeonGreen font-mono">
+      <span class="text-xs text-NeonGreen">
         ADVICE #{{ adviceID }}</span>
     </div>
 
@@ -12,7 +12,7 @@
         "{{ adviceSlip }}"
       </p>
     </div>
-    <div class="flex mb-10">
+    <div class="flex mb-5">
       <!-- <img :src="'./assets/pattern-divider-mobile.svg'" alt="" />
       <img src="./assets/pattern-divider-desktop.svg" alt="" /> -->
       <svg class="mx-auto" width="295" height="16" xmlns="http://www.w3.org/2000/svg">
@@ -27,15 +27,18 @@
 
 
     </div>
-    <button
-      class="group hover:w-16 hover:h-16 w-14 h-14 bg-NeonGreen rounded-full hover:bg-[#16f786] flex items-center absolute bottom-72 md:bottom-32 md:right-[48%] md:left-[48%] right-40"
-      @click="reloadPage()">
-      <svg class="mx-auto group-hover:rotate-45" width="24" height="24" xmlns="http://www.w3.org/2000/svg">
-        <path
-          d="M20 0H4a4.005 4.005 0 0 0-4 4v16a4.005 4.005 0 0 0 4 4h16a4.005 4.005 0 0 0 4-4V4a4.005 4.005 0 0 0-4-4ZM7.5 18a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Z"
-          fill="#202733" />
-      </svg>
-    </button>
+    <div>
+      <button
+        class="w-14 h-14 bg-NeonGreen rounded-full hover:bg-[#befcdd] flex items-center md:absolute md:bottom-32 md:right-[48%] md:left-[48%] mx-auto"
+        @click="reloadPage()">
+        <svg class="mx-auto hover:rotate-45" width="24" height="24" xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="M20 0H4a4.005 4.005 0 0 0-4 4v16a4.005 4.005 0 0 0 4 4h16a4.005 4.005 0 0 0 4-4V4a4.005 4.005 0 0 0-4-4ZM7.5 18a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Z"
+            fill="#202733" />
+        </svg>
+      </button>
+    </div>
+
   </div>
 </template>
 
